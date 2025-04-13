5a6ae2bf1a079cd6adec98cb627832f597001389
Vahe Sargsyan <w33bv.gl@gmail.com>
2025-04-13 20:00:42 +0400

    fix this components

--- ui-pro-plugin/index.js
+++ ui-pro-plugin/index.js
@@ -4,43 +4,43 @@ import { resolve, join } from "path";
 const UI_PRO_DIR = resolve("node_modules", "@nuxt", "ui-pro");
 
 const INDEX_SOURCE_PATH = resolve(
-  "ui-pro-plugin",
-  "pro",
-  "shared",
-	"v.3.0.2",
-  "ui-pro.BZAmqo9Y.cjs",
+    "ui-pro-plugin",
+    "pro",
+    "shared",
+    "v.3.0.2",
+    "ui-pro.BIDAE6aJ.cjs",
 );
 
 const INDEX_DEST_PATH = join(
-  UI_PRO_DIR,
-  "dist",
-  "shared",
-  "ui-pro.BZAmqo9Y.cjs",
+    UI_PRO_DIR,
+    "dist",
+    "shared",
+    "ui-pro.BIDAE6aJ.cjs",
 );
 
 const LICENSE_SOURCE_PATH = resolve(
-  "ui-pro-plugin",
-  "pro",
-  "shared",
-	"v.3.0.2",
-  "ui-pro.DXGwPxta.mjs",
+    "ui-pro-plugin",
+    "pro",
+    "shared",
+    "v.3.0.2",
+    "ui-pro.qTd-WLcu.mjs",
 );
 
 const LICENSE_DEST_PATH = join(
-  UI_PRO_DIR,
-  "dist",
-  "shared",
-  "ui-pro.DXGwPxta.mjs",
+    UI_PRO_DIR,
+    "dist",
+    "shared",
+    "ui-pro.qTd-WLcu.mjs",
 );
 
 async function rewriteLicenseFile() {
   try {
     if (!(await fs.pathExists(UI_PRO_DIR))) {
-      console.log(`ui-pro не найден в node_modules: ${UI_PRO_DIR}`);
+      console.log(`The target directory is not found: ${UI_PRO_DIR}`);
       return;
     }
 
-    console.log(`Найден ui-pro. Перезаписываю файлы...`);
+    console.log(`Target directory found. Overwriting files...`);
 
     const indexContent = await fs.readFile(INDEX_SOURCE_PATH, "utf-8");
     await fs.outputFile(INDEX_DEST_PATH, indexContent);
@@ -48,10 +48,10 @@ async function rewriteLicenseFile() {
     const licenseContent = await fs.readFile(LICENSE_SOURCE_PATH, "utf-8");
     await fs.outputFile(LICENSE_DEST_PATH, licenseContent);
 
-    console.log("✅ Файлы лицензии успешно перезаписаны!");
+    console.log("✅ Files successfully updated!");
   } catch (error) {
-    console.error("❌ Ошибка при перезаписи файлов:", error);
+    console.error("❌ Error while updating the files:", error);
   }
 }
 
-rewriteLicenseFile().then((r) => {});
+rewriteLicenseFile().then(() => {});
